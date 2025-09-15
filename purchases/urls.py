b0e4ac4814fadda847db8a52b9c3c5f5f97cb316
SGJaziri <schneider_258@hotmail.com>
2025-09-15 18:06:33 -0500

    Actualizacion 18.06

--- purchases/urls.py
+++ purchases/urls.py
@@ -1,23 +1,7 @@
 from django.contrib import admin
-
-from rest_framework.routers import DefaultRouter
-from .views import (
-    CategoryViewSet, ProductViewSet, RestaurantViewSet, UnitViewSet,
-    PurchaseViewSet, PurchaseListViewSet, PublicConfigView
-)
 from django.urls import path, include
-from django.contrib import admin
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),   # ← asegura el include correcto
+    path('api/', include('core.urls')),   # todo lo de la app cuelga de /api/
 ]
-
-router = DefaultRouter()
-router.register('categories', CategoryViewSet)
-router.register('products', ProductViewSet)
-router.register('restaurants', RestaurantViewSet)
-router.register('units', UnitViewSet)
-router.register('purchases', PurchaseViewSet)
-router.register('purchase-lists', PurchaseListViewSet)
